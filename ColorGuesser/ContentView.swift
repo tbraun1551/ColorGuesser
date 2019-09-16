e406c674e509890dd32531ae79cdbbebf08bb62b
Thomas Braun <tbraun1551@gmail.com>
2019-09-16 11:30:05 -0400

    Initial Commit

---
+++ ColorGuesser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorGuesser
+//
+//  Created by Thomas Braun on 9/16/19.
+//  Copyright © 2019 Thomas Braun. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
